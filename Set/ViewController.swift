933f33b293b9181c13ff4dc847eb2a817845dae7
Ben Musoke-Lubega <benmusokelubega@princeton.edu>
2019-12-22 15:39:15 -0500

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Benjamin Musoke-Lubega on 12/22/19.
+//  Copyright © 2019 Ben Musoke-Lubega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
